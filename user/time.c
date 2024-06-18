795232d0e31382cd4bc8de46016f19d590009b5e
Johnathon <jdamartinez13@gmail.com>
2024-06-17 23:23:45 -0700

    commit	modified:   user/time.c commit	modified:   Makefile commit	modified:   kernel/param.h commit	modified:   kernel/syscall.c commit	modified:   kernel/syscall.h commit	modified:   kernel/sysproc.c commit	modified:   user/user.h commit	modified:   user/usys.pl commit	user/tmtest.c

--- user/time.c
+++ user/time.c
@@ -1,34 +1,37 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <sys/types.h> 
-#include <unistd.h>
 
 
 int main(int argc, char *argv[]){
 
- float starttime = uptime();
-    
+ int starttime = uptime();
+    printf("start time is: %d\n",starttime);
  int p = fork();    //creates a copy and makes p equal the pid
-
+ 
 if(p<0){
 printf("Fork Failed"); //if fork function fails print error
 exit(1);
 }
 
 if(p==0){
-    //exec(argc, argv);
+    
+    if(argc==1){
+        exit(0);
+    }
+    if(exec(argv[1],argv)== -1){
+        printf("error executing file");
+    };
     
 
 }else{
-    wait(NULL);
-    float stoptime = uptime();
-    float finaltime = stoptime - starttime;
-    printf("%f",finaltime);
+    wait(0);
+    int stoptime = uptime();
+    printf("stop time is: %d\n",stoptime);
+    int finaltime = stoptime - starttime;
+    printf("final time is :%d\n",finaltime);
 }
-
+exit(0);
 
 }
 
